65680497ca660ba84e42839559f69ba8476a3cf0
Srishti613 <1803srishti@gmail.com>
2026-01-31 20:32:26 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,28 @@
+# Calculator Program 
+
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result , 2))
+
+elif operator == "-":
+    result = num1 - num2
+    print(round(result , 2))
+
+elif operator == "*":
+    result = num1 * num2
+    print(round(result , 2))
+
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print(round(result , 2))
+    else:
+        print("Divison by zero is not allowed")    
+
+else:
+    print(f"{operator} is not valid")    
+
